60a6447856f1653c8fbad1f4184f5e524f0a3fc8
narellageorg <130798547+narellageorg@users.noreply.github.com>
2023-05-21 13:12:50 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var nombre = "Narella"
+var apellido = "Georg"
+
+console.log(nombre+" "+apellido)
+
+var manzanas = 30
+var peras = 43
+var suma = (manzanas+peras)
+
+console.log(manzanas+peras+suma)
+
+var otra = (suma+23)
+
+console.log(otra+manzanas)
